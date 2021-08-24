56078f10ec4ae3c9fd305982a5357bd9056582f4
Boris Quiroz <boris@insert-coin.org>
2021-08-24 19:43:18 -0400

    TF code for lambda + iam

---
+++ lambda/lambda.tf
@@ -0,0 +1,37 @@
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_lambda_function" "test" {
+  filename      = "functions/hello_world_py.zip"
+  function_name = "hello_world_py"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "app.py"
+
+  source_code_hash = filebase64sha256("functions/hello_world_py.zip")
+
+  runtime = "python3.8"
+
+  environment {
+    variables = {
+      Name = "Test"
+    }
+  }
+}
---
+++ lambda/variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  default = "mock_access_key"
+}
+
+variable "secret_key" {
+  default = "mock_secret_key"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
