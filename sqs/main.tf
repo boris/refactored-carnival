c44b9a67798f471a48b0d48cea1d8bd582da4134
Boris Quiroz <boris@insert-coin.org>
2021-11-02 17:08:24 -0300

    Add one queue

---
+++ sqs/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
+
+  # Needed for local development
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    sqs = "http://localhost:4566"
+  }
+}
