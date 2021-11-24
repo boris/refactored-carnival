414dfb5c47ec772a57c67402d9579179ebdf6f75
Boris Quiroz <boris@insert-coin.org>
2021-11-23 23:06:38 -0300

    fix method and resources

--- lambda/apigw.tf
+++ lambda/apigw.tf
@@ -11,7 +11,7 @@ resource "aws_api_gateway_resource" "proxy" {
 resource "aws_api_gateway_method" "proxy_method" {
   rest_api_id   = aws_api_gateway_rest_api.test.id
   resource_id   = aws_api_gateway_resource.proxy.id
-  http_method   = "ANY"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
@@ -28,14 +28,14 @@ resource "aws_api_gateway_integration" "lambda" {
 resource "aws_api_gateway_method" "proxy_root" {
   rest_api_id   = aws_api_gateway_rest_api.test.id
   resource_id   = aws_api_gateway_rest_api.test.root_resource_id
-  http_method   = "ANY"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
   rest_api_id = aws_api_gateway_rest_api.test.id
-  resource_id = aws_api_gateway_method.proxy_method.resource_id
-  http_method = aws_api_gateway_method.proxy_method.http_method
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
